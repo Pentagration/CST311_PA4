6d0598752f165e999f15ca43c9f26e69f13aedeb
gonzalezcode <marcus68@protonmail.com>
2020-02-24 20:04:39 -0800

    IP address changes

--- legacy_router.py
+++ legacy_router.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-# File: legacy_router.py
 from mininet.net import Mininet
 from mininet.node import Host, Node
 from mininet.cli import CLI
@@ -9,22 +7,27 @@ def myNetwork():
     net = Mininet( topo=None, build=False, ipBase='10.0.0.0/8')
     info( '*** Adding controller\n' )
     info( '*** Add switches\n')
-    r1 = net.addHost('r1', cls=Node, ip='0.0.0.0')
+    r1 = net.addHost('r1', cls=Node, ip='10.0.1.1/8')
     r1.cmd('sysctl -w net.ipv4.ip_forward=1')
+
+    s1,s2 = [net.addSwitch(s) for s in 's1', 's2']
+    net.addLink(s1,r1, intfName2='r1-eth1',params2={'ip':'10.0.1.1/8'})
+    net.addLink(s2,r1, intfName2='r1-eth2',params2={'ip':'192.168.0.1/24'})
     info( '*** Add hosts\n')
-    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
-    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
-    
+    h1 = net.addHost('h1', cls=Host, ip='10.0.1.100/8', defaultRoute='via 10.0.1.1')
+    h2 = net.addHost('h2', cls=Host, ip='192.168.0.50/24', defaultRoute='via 192.168.0.1')
     info( '*** Add links\n')
-    net.addLink(h1, r1)
-    net.addLink(h2, r1)
-    
+    net.addLink(h1, s1)
+    net.addLink(h2, s2)
+    net.addLink(s1,s2)
+
     info( '*** Starting network\n')
     net.build()
-    
+
     CLI(net)
     net.stop()
-    
+
 if __name__ == '__main__':
     setLogLevel( 'info' )
     myNetwork()
+~
