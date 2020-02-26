b966835467ed2a0f6cb61548c2cfff77ec53c854
Adam Houser <aphouser@gmail.com>
2020-02-26 09:54:27 -0800

    updated legacy_router.py by removing switches added linuxrouter.py for comparison and working

--- legacy_router.py
+++ legacy_router.py
@@ -3,31 +3,25 @@ from mininet.node import Host, Node
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 def myNetwork():
-
-    net = Mininet( topo=None, build=False, ipBase='10.0.0.0/8')
-    info( '*** Adding controller\n' )
-    info( '*** Add switches\n')
-    r1 = net.addHost('r1', cls=Node, ip='10.0.1.1/8')
+    net = Mininet(topo=None, build=False, ipBase='10.0.1.0/24')
+    info('*** Adding controller\n')
+    info('*** Adding switches\n')
+    r1 = net.addHost('r1', cls=Node, ip='10.0.1.1/24')
     r1.cmd('sysctl -w net.ipv4.ip_forward=1')
 
-    s1,s2 = [net.addSwitch(s) for s in 's1', 's2']
-    net.addLink(s1,r1, intfName2='r1-eth1',params2={'ip':'10.0.1.1/8'})
-    net.addLink(s2,r1, intfName2='r1-eth2',params2={'ip':'192.168.0.1/24'})
-    info( '*** Add hosts\n')
-    h1 = net.addHost('h1', cls=Host, ip='10.0.1.100/8', defaultRoute='via 10.0.1.1')
-    h2 = net.addHost('h2', cls=Host, ip='192.168.0.50/24', defaultRoute='via 192.168.0.1')
-    info( '*** Add links\n')
-    net.addLink(h1, s1)
-    net.addLink(h2, s2)
-    net.addLink(s1,s2)
+    info('*** Add host\n')
+    h2 = net.addHost('h2', cls=Host, ip='10.0.1.2/24', defaultRoute='via 10.0.1.1')
+    h1 = net.addHost('h1', cls=Host, ip='10.0.2.2/24', defaultRoute='via 10.0.2.1')
 
-    info( '*** Starting network\n')
+    info('***Add links\n')
+    net.addLink(h2, r1, intfName2='r1-eth1', params2={'ip': '10.0.1.1/24'})
+    net.addLink(h1, r1, intfName2='r1-eth2', params2={'ip': '10.0.2.1/24'})
+
+    info('*** Starting network\n')
     net.build()
-
     CLI(net)
     net.stop()
 
 if __name__ == '__main__':
-    setLogLevel( 'info' )
+    setLogLevel('info')
     myNetwork()
-~
